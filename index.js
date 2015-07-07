0736fd86b0cbccc522b587b7f8c87f2619f2c050
alfred sang <i5ting@126.com>
2015-07-07 12:37:11 +0800

    init

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var program = require('commander');
 program
   .version('0.0.1')
   .command('init [otherDirs...]')
-  .action(init)
+  .action(require('./lib/init'))
   .command('rmdir <dir> [otherDirs...]')
   .action(function (dir, otherDirs) {
     console.log('rmdir %s', dir);
@@ -22,36 +22,3 @@ program
 
 program.parse(process.argv);
 
-function init(){
-  
-  require('shelljs/global');
-  
-  if (!which('git')) {  
-    echo('Sorry, this script requires git');  
-    exit(1);
-  }
-  
-  // Run external tool synchronously
-  if (exec('git commit -am "Auto-commit"').code !== 0) {
-    echo('Error: Git commit failed');
-    exit(1);
-  }
-  
-  var clone = 'git clone --depth=1 https://github.com/moajs/nmm-tmpl'
-  // Run external tool synchronously
-  if (exec(clone).code !== 0) {
-    echo('Error: Git clone failed');
-    exit(1);
-  }else{
-    echo('Success: exn clone finished!');
-  }
-  
-  var clone_post = 'rm -rf nmm-tmpl/.git && cp -rf nmm-tmpl/* . && rm -rf nmm-tmpl';
-  if (exec(clone_post).code !== 0) {
-    echo('Error: Git clone_post failed');
-    exit(1);
-  }else{
-    echo('Success: exn clone_post finished!');
-  }
-  
-}
